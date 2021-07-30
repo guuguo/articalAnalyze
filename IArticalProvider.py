84100fd5a2017f18237487a8fa8021c6b8850cb6
guuguo <wb-gdq879913@alibaba-inc.com>
2021-07-30 18:14:54 +0800

    move bean

--- IArticalProvider.py
+++ IArticalProvider.py
@@ -1,46 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-class ArticleType(object):
-    """文章类型类"""
-
-    def __init__(self):
-        self.title = ''
-        self.href = ''
-        self.id = ''
-
-    def __str__(self):
-        return 'href：' + self.href + "\n" + 'title：' + self.title + "\n" + 'id：' + self.id
-
-
-class Article(object):
-    """文章类"""
-
-    def __init__(self):
-        self.title = ''
-        self.href = ''
-        self.authorID = ''
-        self.authorName = ''
-        # 作者链接
-        self.authorHref = ''
-        # 类型
-        self.types = ''
-        # 文章内容
-        self.content = ''
-        # 阅读数
-        self.read = ''
-        # 评价数
-        self.comment = ''
-        # 收藏数
-        self.collect = ''
-        # 是否全部信息
-        self.isAllInfo = ''
-        # 简介
-        self.brief = ''
-
-    def __str__(self):
-        return 'href：' + self.href + "\n" + 'title：' + self.title
+from Bean import ArticleType, Article
 
 
 class JuejinArticleProvider(object):
